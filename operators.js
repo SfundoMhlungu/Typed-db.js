ee81b3f52e0b6f876888711a925ad71ccc5f238b
sfundomhlungu <devadress21@gmail.com>
2022-07-08 03:19:21 +0200

    CRUD functions

---
+++ operators.js
@@ -0,0 +1,45 @@
+import {isInDb} from "./utils.js"
+import select from "./select.js"
+
+
+
+
+export function insert(row){
+            this.store.insert(row)
+
+	 // try{
+	 //   if(this.meta.options.timeStamp){
+	 //      row["timeStamp"] = Date.now()
+	 //   }
+		 // this.store[this.meta.length] = row
+		 // this.meta.length++
+		   // return true
+	 // }catch(err){
+		  
+          // return false
+
+
+	// }
+
+
+}
+
+
+
+
+
+
+
+export function delete_(){
+
+
+
+}
+
+
+export function update(){
+
+
+
+};
+
